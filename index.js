c2cc3c2ad01a5434e6cfce28e51827794934ad2a
Alexiszxc22 <Alexiszxc22>
2024-03-14 15:49:00 +0800

    Bingo HTML

--- index.js
+++ index.js
@@ -1,20 +1,40 @@
 console.log("Hello World!");
 
-function generateRandomNumber () {
+generateNumber(75);
 
-    let RandomNum = Math.floor(Math.random() * 10) + 4;
+function generateNumber(max) {
+   // Add = +
+   // Sub = -
+   // Div = /
+   // Mult = *
+   // Rem = %
+   let randomNumber = Math.floor(Math.random() * max) + 1;
 
-// if ...else if...else statement
-//if (condition) {block of code to execute}
-// =, !, =, >, <, > =, < =, &&, ||
+  // if ...else if...else statement
+  //if (condition) {block of code to execute}
+  // =, !, =, >, <, > =, < =, &&, ||
 
+   if (randomNumber <= 15) {
+    return console.log(`Random Number ${randomNumber} is in B. 1 - 15`);
+      
+   } else if (randomNumber >= 16 && randomNumber <= 30) {
+      return console.log(`Random Number ${randomNumber} is in I. 16 - 30`);
+      
+   } else if (randomNumber >= 31 && randomNumber <= 45) {
+    return console.log(`Random Number ${randomNumber} is in N. 31 - 45`);
+   }
+   else if (randomNumber >= 46 && randomNumber <= 55) {
+    return console.log(`Random Number ${randomNumber} is in G. 46 - 55`);
+   }
+   else if (randomNumber >= 56 && randomNumber <= 75) {
+    return console.log(`Random Number ${randomNumber} is in O. 55 - 75`);
+   }
+   else {
+      console.log('The number is invalid.')
+   }
 
-
-    if(RandomNum >= 5) {
-        console.log(`RandomNumber is > = to 5`);
-    } else {
-        console.log(`RandomNumber is < = to 5`);
-    }
-
-    return console.log(RandomNum);
+   return console.log(randomNumber);
 }
+
+
+
